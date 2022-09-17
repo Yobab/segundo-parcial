a477c1c5f34a109b377b1a9b53479c544c630616
Yobab <yjavier@miumg.edu.gt>
2022-09-17 12:33:32 -0600

    final commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,45 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\student;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function getAll()
     {
-        return student::all();
+        return Student::all();
     }
     public function index()
     {
-        $students = student::all();
+        $students = Student::all();
         return view('welcome', compact('students'));
     }
     public function delete($id)
     {
-        return student::find($id)->delete();
+        return Student::find($id)->delete();
     }
     public function edit(Request $request, $id)
     {
         $data = $request->validate([
-            'name' => 'required',
-            'isbn' => 'required',
-            'year' => 'required',
-            'genre' => 'required',
-            'author' => 'required',
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'edad' => 'required',
+            'grado' => 'required',
+            'sexo' => 'required',
         ]);
-        student::find($id)->update($data);
+        Student::find($id)->update($data);
     }
     public function create(Request $request)
     {
         $data = $request->validate([
-            'name' => 'required',
-            'isbn' => 'required',
-            'year' => 'required',
-            'genre' => 'required',
-            'author' => 'required',
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'edad' => 'required',
+            'grado' => 'required',
+            'sexo' => 'required',
         ]);
-        student::insert($data);
+        Student::insert($data);
     }
 }
-
